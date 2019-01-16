71b3d3156f8ca18d793f2c580466b5bcadf151f8
mato <tomoya.matou@gmail.com>
2019-01-17 00:04:49 +0900

    add store

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 
+// ストアをインポートする
+import store from './store'
+
 // ルーティングの定義をインポートする
 import router from './router'
 
@@ -9,6 +12,7 @@ import App from './App.vue'
 new Vue({
     el: '#app',
     router, // ルーティングの定義を書き込む
+    store,
     components: { App }, // ルートコンポーネントの使用を宣言する
     template: '<App />'  // ルートコンポーネントを描画する
 })
