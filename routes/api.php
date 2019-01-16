7676cfaa000dc8f29112793dae13a221aaaa7dee
mato <tomoya.matou@gmail.com>
2019-01-16 22:05:15 +0900

    create LoginApi

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,7 @@
 <?php
 
+// 会員登録
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
+
+// ログイン
+Route::post('/login', 'Auth\LoginController@login')->name('login');
--- tests/Feature/RegisterApiTest.php
+++ tests/Feature/RegisterApiTest.php
@@ -14,6 +14,7 @@ class RegisterApiTest extends TestCase
     /**
      * @test
      */
+
     public function should_新しいユーザーを作成して返却する()
     {
         $data = [
