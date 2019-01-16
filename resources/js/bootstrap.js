dc6ec840b446a4ef3487ab1922fb1809d2140896
mato <tomoya.matou@gmail.com>
2019-01-17 00:22:29 +0900

    for csrf

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,3 +1,19 @@
+import {
+    getCookieValue
+} from './util'
+
+window.axios = require('axios');
+
+// Ajaxリクエストであることを示すヘッダーを付与する
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.axios.interceptors.request.use(config => {
+    // クッキーからトークンを取り出してヘッダーに添付する
+    config.headers['X-XSRF-TOKEN'] = getCookieValue('XSRF-TOKEN');
+
+    return config;
+})
+
 
 window._ = require('lodash');
 
