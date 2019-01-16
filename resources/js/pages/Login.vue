288a75fda1cb66d8ee70578e57e316300a40e0f3
mato <tomoya.matou@gmail.com>
2019-01-17 00:45:41 +0900

    create register

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -54,8 +54,12 @@
             login () {
                 console.log(this.loginForm)
             },
-            register () {
-                console.log(this.registerForm)
+            async register () {
+                // authストアのregisterアクションを呼び出す
+                await this.$store.dispatch('auth/register', this.registerForm)
+
+                // トップページに移動する
+                this.$router.push('/')
             }
         }
     }
