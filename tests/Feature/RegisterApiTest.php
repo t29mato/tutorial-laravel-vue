239dc28393f85cad5f7c60b26a01acc23b647a9f
mato <tomoya.matou@gmail.com>
2019-01-16 21:52:33 +0900

    create  RegisterApi

--- tests/Feature/RegisterApiTest.php
+++ tests/Feature/RegisterApiTest.php
@@ -2,19 +2,32 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class RegisterApiTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
-     * A basic test example.
-     *
-     * @return void
+     * @test
      */
-    public function testExample()
+    public function should_新しいユーザーを作成して返却する()
     {
-        $this->assertTrue(true);
+        $data = [
+            'name' => 'moray eel',
+            'email' => 'moray@gmail.com',
+            'password' => 'test1234',
+            'password_confirmation' => 'test1234',
+        ];
+
+        $response = $this->json('POST', route('register'), $data);
+
+        $user = User::first();
+        $this->assertEquals($data['name'], $user->name);
+
+        $response->assertStatus(201)->assertJson(['name' => $user->name]);
     }
 }
