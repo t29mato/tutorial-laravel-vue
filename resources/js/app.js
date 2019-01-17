16202c457939b958a69cba7632eb4224e8c915fa
mato <tomoya.matou@gmail.com>
2019-01-17 21:27:01 +0900

    keep login status

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,10 +10,16 @@ import router from './router'
 // ルートコンポーネントをインポートする
 import App from './App.vue'
 
-new Vue({
-    el: '#app',
-    router, // ルーティングの定義を書き込む
-    store,
-    components: { App }, // ルートコンポーネントの使用を宣言する
-    template: '<App />'  // ルートコンポーネントを描画する
-})
+const createApp = async() => {
+    await store.dispatch('auth/currentUser')
+
+    new Vue({
+        el: '#app',
+        router, // ルーティングの定義を書き込む
+        store,
+        components: { App }, // ルートコンポーネントの使用を宣言する
+        template: '<App />'  // ルートコンポーネントを描画する
+    })
+}
+
+createApp()
